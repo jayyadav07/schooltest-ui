ddf851b2cc2f974a95ae72d93fe77d2ca4ceaea7
Jay Yadav <jayyadav1207@gmail.com>
2025-01-10 00:26:33 +0530

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { SidebarMenuComponent } from './shared/components/sidebar-menu/sidebar-menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet,
+    CommonModule,
+    RouterModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    SidebarMenuComponent
+
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/components/sidebar-menu/sidebar-menu.component.ts
+++ src/app/shared/components/sidebar-menu/sidebar-menu.component.ts
@@ -1,11 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterModule } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { MenuItem } from '../../../core/models/menu-item.model';
+import { MenuService } from '../../../core/services/menu.service';
+
 
 @Component({
   selector: 'app-sidebar-menu',
-  imports: [],
+  standalone: true,
+  imports: [
+      CommonModule,
+      RouterModule,
+      MatSidenavModule,
+      MatListModule,
+      MatIconModule,
+      MatExpansionModule],
   templateUrl: './sidebar-menu.component.html',
+  animations: [
+    trigger('slideInOut', [
+      state('void', style({
+        transform: 'translateX(-100%)'
+      })),
+      state('*', style({
+        transform: 'translateX(0)'
+      })),
+      transition('void <=> *', animate('200ms ease-in-out'))
+    ])
+  ],
   styleUrl: './sidebar-menu.component.scss'
 })
 export class SidebarMenuComponent {
 
+  menuItems: MenuItem[];
+
+  constructor(menuService: MenuService) {
+    this.menuItems = menuService.getMenuItems();
+  }
 }
